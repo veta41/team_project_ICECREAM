56ca7b39812d7d720f04c49d61b56c1669a828d6
morysol <cepera.cMupHoB@gmail.com>
2022-04-04 00:19:08 +0300

    night

--- src/js/read-more-modal.js
+++ src/js/read-more-modal.js
@@ -1,6 +1,16 @@
-const testButton = document.querySelector('.btn-about');
+const readmoreButton = document.querySelector('.btn-about');
 const overlay = document.querySelector('.overlay');
 const modal = document.querySelector('.readmore__modal');
 
-testButton.addEventListener('click', () => overlay.classList.toggle('overlay--off'));
-overlay.addEventListener('click', () => overlay.classList.toggle('overlay--off'));
+const maxWidth = document.body.clientWidth;
+
+readmoreButton.addEventListener('click', () => {
+  overlay.classList.remove('overlay--off');
+  //   console.log(document.body.clientWidth);
+  document.body.setAttribute('style', `max-width: ${maxWidth}px; overflow: hidden;`);
+});
+overlay.addEventListener('click', () => {
+  overlay.classList.add('overlay--off');
+  //   console.log(document.body.clientWidth);
+  document.body.removeAttribute('style');
+});
