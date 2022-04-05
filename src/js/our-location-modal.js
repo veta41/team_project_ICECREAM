226261234788f7417244363a9ed58e00172acfcc
morysol <cepera.cMupHoB@gmail.com>
2022-04-05 18:21:35 +0300

    fix1

--- src/js/our-location-modal.js
+++ src/js/our-location-modal.js
@@ -4,14 +4,30 @@
     closeModalBtn: document.querySelector('[data-modal-map-close]'),
     modal: document.querySelector('[data-modal-map]'),
   };
-const maxWidth = document.body.clientWidth;
+
+  const maxWidth = document.body.clientWidth;
+
   // refs.openModalBtn.addEventListener('click', toggleModal);
   // refs.closeModalBtn.addEventListener('click', toggleModal);
- // refs.openModalBtn.addEventListener('click', toggleModal);
-  // refs.closeModalBtn.addEventListener('click', toggleModal);
 
-  function toggleModal() {
-    document.body.classList.toggle('modal-map-open');
-    refs.modal.classList.toggle('is-hidden');
+  refs.openModalBtn.addEventListener('click', addModal);
+  refs.closeModalBtn.addEventListener('click', removeModal);
+
+  function addModal() {
+    // document.body.classList.add('modal-map-open');
+    refs.modal.classList.remove('is-hidden');
+    console.log('ddddddddddddddddddd');
+    document.body.setAttribute('style', `max-width: ${maxWidth}px; overflow: hidden;`);
   }
+
+  function removeModal() {
+    document.body.classList.remove('modal-map-open');
+    refs.modal.classList.add('is-hidden');
+    document.body.removeAttribute('style');
+  }
+
+  // function toggleModal() {
+  //   document.body.classList.toggle('modal-map-open');
+  //   refs.modal.classList.toggle('is-hidden');
+  // }
 })();
